2d8c822270283bf1fd4b008ad52f17405783790d
Technology-maker <ys5401519@gmail.com>
2025-01-15 13:41:43 +0530

    update file

--- practice.js
+++ practice.js
@@ -1,1 +1,13 @@
-console.log('hello');
+let str = "satender";
+str = str + 23;
+console.log(str);
+console.log(typeof str);
+
+const details = {
+    name: "satender",
+    corse: "BCA",
+    section: "a",
+}
+details['email'] = "satenderyadav301019@gmail.com";
+console.log(details);
+console.log(details.name);
