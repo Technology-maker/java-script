f18a2a00968d1015cd4fda98eadf686ce9c0238a
Technology-maker <ys5401519@gmail.com>
2025-01-28 01:11:05 +0530

    new files

--- 28-HTML Insertion Methodss.js
+++ 28-HTML Insertion Methodss.js
@@ -1,5 +1,8 @@
 let a = document.getElementsByTagName("div")[0];
 a.innerHTML = a.innerHTML + '<h1>Hello raoshab</h1>';
+
 let crt = document.createElement('div');
 crt.innerHTML = '<h2>hello ji</h2>';
-crt.appendChild(crt);
+// crt.appendChild(crt);
+// crt.prepend(crt);
+crt.append(crt);
---
+++ 29-insert-adjustant-element.js
@@ -0,0 +1,8 @@
+let first = document.getElementById("first");
+
+// first.insertAdjacentHTML("beforebegin", '<div class ="inner"> hey i am a inner container</div>')
+// first.insertAdjacentHTML("beforeend", '<div class ="inner"> hey i am a inner container</div>')
+// first.insertAdjacentHTML("afterbegin", '<div class ="inner"> hey i am a inner container</div>')
+// first.insertAdjacentHTML("afterend", '<div class ="inner"> hey i am a inner container</div>');
+
+first.remove();
